59fc50eec2f2c4e9e24e4234af1aed21e062bca7
Bradley Stender <bradleystender@gmail.com>
2018-02-01 13:45:04 -0500

    Creating project

---
+++ src/com/company/Pencil.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Pencil {
+
+    private double leadSize;
+    private String brand;
+    private boolean hasEraser;
+    private String color;
+    private boolean isMechanical;
+
+    public double getLeadSize() {
+        return leadSize;
+    }
+
+    public void setLeadSize(double leadSize) {
+        this.leadSize = leadSize;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public boolean isHasEraser() {
+        return hasEraser;
+    }
+
+    public void setHasEraser(boolean hasEraser) {
+        this.hasEraser = hasEraser;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isMechanical() {
+        return isMechanical;
+    }
+
+    public void setMechanical(boolean mechanical) {
+        isMechanical = mechanical;
+    }
+}
